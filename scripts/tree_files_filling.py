fa143c3416d9f5efeb0b0a2ea0d4b1784052b9f3
Tatiana Bellagio <tatianabellagio@gmail.com>
2025-08-17 19:56:05 -0700

    add debug

--- scripts/tree_files_filling.py
+++ scripts/tree_files_filling.py
@@ -1,12 +1,14 @@
 import os
 
 output_gen6_trees = snakemake.output['sim_tree_gen6'] 
+print(output_gen6_trees)
+print(os.path.exists(output_gen6_trees))
 
-def create_empty_file_if_not_exists(filename):
-    if not os.path.exists(filename):
+def create_empty_file_if_not_exists(output_gen6_trees):
+    if not os.path.exists(output_gen6_trees):
         # Create an empty file
-        print(f"Creating empty file: {filename}")
-        with open(filename, 'w') as file:
+        print(f"Creating empty file: {output_gen6_trees}")
+        with open(output_gen6_trees, 'w') as file:
             pass
 
 create_empty_file_if_not_exists(output_gen6_trees)
