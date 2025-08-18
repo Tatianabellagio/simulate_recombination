1b57524de7a435a612a040c175004cabeb412513
Tatiana Bellagio <tatianabellagio@gmail.com>
2025-08-17 19:26:22 -0700

    add debug

--- scripts/slim.sh
+++ scripts/slim.sh
@@ -19,6 +19,7 @@ output_maxphenotype="${snakemake_output[output_maxphenotype]}"
 output_minphenotype="${snakemake_output[output_minphenotype]}"
 
 output_tree_gen6="${snakemake_params[output_tree_gen6]}"
+echo "output_tree_gen6 = ${snakemake_params[output_tree_gen6]}"
 
 # Map 'selection' to its numeric value using a case statement
 case "$selection" in
